1afcb8279614b36e08cd7f913199cdf288c30231
Benki <wangyawei9999@gmail.com>
2017-06-03 15:25:28 +0800

    first commit

---
+++ module-one/src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/6/3.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("-------------------------");
+    }
+}
